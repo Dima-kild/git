ac5810049c993ff1aff9280a0b6f0f85c35972cf
Dima <di.kild_0510@mail.ru>
2022-01-04 23:04:30 +0300

    add new class

---
+++ src/entity/first.java
@@ -0,0 +1,11 @@
+package entity;
+
+public class first {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+
+        human human = new human("Dima", 28);
+        System.out.println(human.toString());
+    }
+
+}
